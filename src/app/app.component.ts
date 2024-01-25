b846b61e8b3d1c619065c9879e1456bcc92f051d
eljily <eljilysidibrahim@gmail.com>
2024-01-25 22:42:13 +0000

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  currentAction: any;
-setCurrentaction(a:any) {
-  this.currentAction=a;
-}
-  actions : Array<any>=[
-    {title:"Home",route:"/home",icon:"house"},
-    {title:"Products",route:"/products",icon:"arrow-up"},
-    {title:"New Product",route:"/newProduct",icon:"plus"}
-  ]
+ 
 }
 
